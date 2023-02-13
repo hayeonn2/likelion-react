ed888022d65c135df92a7ff4cadc5517ce71fc3c
hayoenn2 <dwd1205@naver.com>
2023-02-13 14:31:21 +0900

    [Refac] createElement 함수 활용 코드

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,23 +1,50 @@
 import "./utils/supportJavaScript.js";
-import "./vanilla.script.js";
-
-// console.log('React 개발 시작하기! 😊');
+// import './vanilla.script.js';
 
 // 명령형 프로그래밍
-// const root = document.getElementById("root");
-// const headline = root.querySelector("hi");
+// const root = document.getElementById('root');
+// const headline = root.querySelector('h1');
 
-// document.addEventListener("click", () => {
-// headeline.textContent = "update headline content";
+// document.addEventListener('click', () => {
+//   headline.textContent = 'update headline content';
 // });
 
+// React가 제공
 // 선언형 프로그래밍
-import { state, render, update } from "./vanilla.script.js";
 
-render(state);
+// 범쌤 JavaScript
+// 바닐라 프로젝트 (명령형 프로그래밍)
+import { createElement, state, render, update } from "./vanilla.script.js";
+
+const app = createElement(
+  "section",
+  {
+    class: "app",
+    "aria-label": "Like React App",
+  },
+  createElement("h1", {}, "hello App"),
+  createElement("p", {}, "blab blab blab"),
+  createElement(
+    "ul",
+    {},
+    createElement("li", {}, "item 1"),
+    createElement("li", {}, "item 2"),
+    createElement("li", {}, "item 3")
+  )
+);
+
+document.getElementById("root").append(app);
+
+// console.log(state);
+
+// render(state);
+
 document.addEventListener("click", () => {
   update({
-    subjects: "React는 선언형 프로그래밍을 추구합니다.",
+    subjects: "React는 선언형 프로그래밍 방식을 사용합니다.",
   });
-  console.log(state);
+
+  // console.log(state);
 });
+
+// console.log('React 개발 시작하기! 😊');
