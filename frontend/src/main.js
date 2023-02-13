fd73b675f45f688c95ef8735cfe4d3f0b2628c86
hayoenn2 <dwd1205@naver.com>
2023-02-13 14:26:43 +0900

    [Learn] 유틸리티 함수

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,23 @@
-import './utils/supportJavaScript.js';
+import "./utils/supportJavaScript.js";
+import "./vanilla.script.js";
 
-console.log('React 개발 시작하기! 😊');
+// console.log('React 개발 시작하기! 😊');
+
+// 명령형 프로그래밍
+// const root = document.getElementById("root");
+// const headline = root.querySelector("hi");
+
+// document.addEventListener("click", () => {
+// headeline.textContent = "update headline content";
+// });
+
+// 선언형 프로그래밍
+import { state, render, update } from "./vanilla.script.js";
+
+render(state);
+document.addEventListener("click", () => {
+  update({
+    subjects: "React는 선언형 프로그래밍을 추구합니다.",
+  });
+  console.log(state);
+});
