83736ad55558af13d3bf92442157b2478ddbb329
hayoenn2 <dwd1205@naver.com>
2023-02-21 11:23:48 +0900

    refac | index.js 변경

--- counter-with-cra/src/app/App.jsx
+++ counter-with-cra/src/app/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
-import { Counter } from "../components";
+import { CounterFunction, CounterClass } from "../components";
 
 function App() {
   return (
     <div className="App">
-      <Counter count={2} />
-      <Counter step={3} />
+      <h2>함수 컴포넌트</h2>
+      <CounterFunction count={2} />
+      <h2>클래스 컴포넌트</h2>
+      <CounterClass step={3} />
     </div>
   );
 }
