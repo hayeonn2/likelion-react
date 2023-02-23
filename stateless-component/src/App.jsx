126f4537227f2d097579b8b65bfe7f7bb7b83252
hayoenn2 <dwd1205@naver.com>
2023-02-23 09:54:09 +0900

    add | className 학습

--- stateless-component/src/App.jsx
+++ stateless-component/src/App.jsx
@@ -14,12 +14,24 @@ function App() {
         <Button secondary>로그인</Button>
         {/* 컴포넌트 자체를 태그로 만들어준다! 접근성 관련. 핵심! 아주관련!! as라는 프롭이 가진 능력이 굉장히 중요하다.
         재사용성을 높여 작업이 가능하다.*/}
-        <A11yHidden as="h2">저장</A11yHidden>
-        <A11yHidden as="a" skipToContent>
-          저장
-        </A11yHidden>
-        <A11yHidden as="figcaption">저장</A11yHidden>
       </div>
+      <section>
+        <A11yHidden as="h2" className="myClassName">
+          접근성 준수한 제목
+        </A11yHidden>
+        <p>
+          Lorem Ipsum is simply <a href="">dummy</a> text of the printing and
+          typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book.
+        </p>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </p>
+      </section>
     </div>
   );
 }
--- stateless-component/src/components/A11yHidden/A11yHidden.jsx
+++ stateless-component/src/components/A11yHidden/A11yHidden.jsx
@@ -7,9 +7,10 @@ import classes from "./A11yHidden.module.css";
 export const A11yHidden = ({
   // as는 렌더링 할 컴포넌트 요소의 (태그) 이름
   // : 뒤에 쓰는 이름이 별칭(지역 변수가 된다.) 트릭!! 속임수이다. 마치 리액트 컴포넌트이름인 것 처럼..
-  as: Component,
+  as: Component = "span",
   // 비표준속성이므로 restProps에서 제외해야 한다. 이런걸 지켜서 설계하자!
-  focusable,
+  focusable = false,
+  className,
   ...restProps
 }) => {
   // h 함수는 React.createElement의 별칭 함수이다.
@@ -20,6 +21,13 @@ export const A11yHidden = ({
 
   //console.log(restProps); // as 비표준 속성을 표준요소 속성인거마냥 html안에 넣어준다. 리액트는 이걸몰라!
   // 그래서 as를 빼내야 한다.
-  return <Component className={classes.srOnly} {...restProps} />;
+  const combinedClassName = `${classes.srOnly} ${className}`.trim();
+  return <Component className={combinedClassName} {...restProps} />;
   //return h(as, restProps);
 };
+
+// props (외부에서 전달되는 인자 집합)
+// 함수 내부에서는 기본값 설정
+/* A11yHidden.defaultPops = {
+  as: "span",
+}; */
